db0e4e97672efeb6caeb661b323fe4e8adb9583a
Pampaj <dawidkawinski@opoczta.pl>
2023-11-28 22:22:03 +0100

    upgrade about me section

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const dropdownBtn = document.querySelector(".dropdown-btn")
 const dropdownMenu = document.querySelector(".dropdown-menu")
 const dropdownLink = document.querySelectorAll(".dropdown-menu a")
-
+const aboutIcon = document.querySelectorAll(".about-icons i")
 
 dropdownBtn.addEventListener('click', () => {
     dropdownMenu.classList.toggle('show')
@@ -10,6 +10,18 @@ dropdownBtn.addEventListener('click', () => {
 dropdownLink.forEach(item => {
     item.addEventListener('click', () => {
         dropdownMenu.classList.toggle('show')
+    })
+})
+
+aboutIcon.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        item.classList.add('fa-bounce')
+    })
+})
+
+aboutIcon.forEach(item => {
+    item.addEventListener('mouseout', () => {
+        item.classList.remove('fa-bounce')
     })
 })
 
